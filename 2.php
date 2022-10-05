2083bfa6dbbba8feedc701479f90972f4e992b63
LinssunAlGr03 <alina.grr03@gmail.com>
2022-10-05 22:26:37 +0300

    Update 2.php

--- 2.php
+++ 2.php
@@ -4,14 +4,33 @@ echo sumTime($argv[1], $argv[2]);
 
 function sumTime(string $term1, string $term2) : string
 {
-    for ($i = 0; $i <= strlen($term1); $i += 3) {
-        if (is_numeric(substr($term1, $i, 2)) === false || is_numeric(substr($term2, $i, 2)) === false) {
-            return("Ошибка в полученных данных\n\n");
+
+    $symbols = "1234567890:";
+
+    for ($i = 0; $i <= strlen($term1); $i++) {
+        for ($u = 0; $u <= strlen($symbols); $u++) {
+            if (substr($term1, $i, 1) == substr($symbols, $u, 1)) {
+                break;
+            }
+            if ($u === strlen($symbols)) {
+                return "Ошибка";
+            }
         }
     }
 
-    $result = strtotime($term1) + strtotime($term2);
-    $result = date('H:i:s', $result);
+    for ($i = 0; $i <= strlen($term2); $i++) {
+        for ($u = 0; $u <= strlen($symbols); $u++) {
+            if (substr($term2, $i, 1) == substr($symbols, $u, 1)) {
+                break;
+            }
+            if ($u === strlen($symbols)) {
+                return "Ошибка";
+            }
+        }
+    }
 
-    return $result;
+
+    $result = strtotime($term1) + strtotime($term2);
+    return date('H:i:s', $result);
+
 }
