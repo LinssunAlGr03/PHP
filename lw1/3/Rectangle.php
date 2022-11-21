ca321f259b5a22b4c74b18e3c0bcde958c997d96
LinssunAlGr03 <alina.grr03@gmail.com>
2022-11-21 21:42:32 +0300

    Create Rectangle.php

---
+++ lw1/3/Rectangle.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("Shape.php");
+
+abstract class Rectangle extends Shape
+{
+    private float $side1, $side2;
+
+    function __construct(float $var1, float $var2)
+    {
+        $this->side1 = $var1;
+        $this->side2 = $var2;
+        if ($this->side1 <= 0 || $this->side2 <= 0) {
+            throw new Exception("Ошибка в размерах");
+        }
+    }
+
+    public function getPerimeter(): float
+    {
+        return ($this->side1 + $this->side2) * 2;
+    }
+
+    public function getArea(): float
+    {
+        return $this->side1 * $this->side2;
+    }
+}
